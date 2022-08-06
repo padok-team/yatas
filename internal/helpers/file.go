725dbad5a9797ca1f5fc62c10457b32b85f8a96b
StanGirard <stanislasg@padok.fr>
2022-08-06 18:00:52 +0200

    feat(go): updated to 1.19

--- internal/helpers/file.go
+++ internal/helpers/file.go
@@ -5,7 +5,7 @@ import (
 	"os"
 )
 
-//Read a file and return its content as a byte array
+// Read a file and return its content as a byte array
 func ReadFile(configPath string) ([]byte, error) {
 	file, err := os.Open(configPath)
 	if err != nil {
