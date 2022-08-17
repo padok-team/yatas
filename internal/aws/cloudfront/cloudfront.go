b036937f5a58d55b41578e205f0fc2bf7c45ff40
StanGirard <stanislasg@padok.fr>
2022-08-17 22:10:52 +0200

    test(cloudfront): added cookie logging test

--- internal/aws/cloudfront/cloudfront.go
+++ internal/aws/cloudfront/cloudfront.go
@@ -10,24 +10,6 @@ import (
 	"github.com/stangirard/yatas/internal/yatas"
 )
 
-func CheckIfCookieLogginEnabled(checkConfig yatas.CheckConfig, d []SummaryToConfig, testName string) {
-	logger.Info(fmt.Sprint("Running ", testName))
-	var check results.Check
-	check.InitCheck("Cookies Logging Enabled", "Check if all cloudfront distributions have cookies logging enabled", testName)
-	for _, cc := range d {
-		if cc.config.Logging != nil && *cc.config.Logging.Enabled && *cc.config.Logging.IncludeCookies {
-			Message := "Cookie logging is enabled on " + *cc.summary.Id
-			result := results.Result{Status: "OK", Message: Message, ResourceID: *cc.summary.Id}
-			check.AddResult(result)
-		} else {
-			Message := "Cookie logging is not enabled on " + *cc.summary.Id
-			result := results.Result{Status: "FAIL", Message: Message, ResourceID: *cc.summary.Id}
-			check.AddResult(result)
-		}
-	}
-	checkConfig.Queue <- check
-}
-
 func CheckIfACLUsed(checkConfig yatas.CheckConfig, d []SummaryToConfig, testName string) {
 	logger.Info(fmt.Sprint("Running ", testName))
 	var check results.Check
---
+++ internal/aws/cloudfront/cloudfrontCookieLogging.go
@@ -0,0 +1,27 @@
+package cloudfront
+
+import (
+	"fmt"
+
+	"github.com/stangirard/yatas/internal/logger"
+	"github.com/stangirard/yatas/internal/results"
+	"github.com/stangirard/yatas/internal/yatas"
+)
+
+func CheckIfCookieLogginEnabled(checkConfig yatas.CheckConfig, d []SummaryToConfig, testName string) {
+	logger.Info(fmt.Sprint("Running ", testName))
+	var check results.Check
+	check.InitCheck("Cookies Logging Enabled", "Check if all cloudfront distributions have cookies logging enabled", testName)
+	for _, cc := range d {
+		if cc.config.Logging != nil && *cc.config.Logging.Enabled && *cc.config.Logging.IncludeCookies {
+			Message := "Cookie logging is enabled on " + *cc.summary.Id
+			result := results.Result{Status: "OK", Message: Message, ResourceID: *cc.summary.Id}
+			check.AddResult(result)
+		} else {
+			Message := "Cookie logging is not enabled on " + *cc.summary.Id
+			result := results.Result{Status: "FAIL", Message: Message, ResourceID: *cc.summary.Id}
+			check.AddResult(result)
+		}
+	}
+	checkConfig.Queue <- check
+}
