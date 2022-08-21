5128b720850e6f49f73cdd56e6deb845ab23cfd5
StanGirard <stanislasg@padok.fr>
2022-08-21 21:02:55 +0200

    test(apigateway): added better tests

--- internal/aws/apigateway/getter_test.go
+++ internal/aws/apigateway/getter_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"reflect"
 	"testing"
+	"time"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/apigateway"
@@ -14,22 +15,47 @@ type mockGetApiGateways func()
 
 func (m mockGetApiGateways) GetRestApis(ctx context.Context, input *apigateway.GetRestApisInput, optFns ...func(*apigateway.Options)) (*apigateway.GetRestApisOutput, error) {
 	// Return an empty list of API Gateway instances
+	timeTest, _ := time.Parse("2006-01-02T15:04:05Z", "2019-01-01T00:00:00Z")
 	return &apigateway.GetRestApisOutput{
-		Items: []types.RestApi{},
+		Items: []types.RestApi{
+			{
+				Id:                     aws.String("id"),
+				CreatedDate:            aws.Time(timeTest),
+				Name:                   aws.String("name"),
+				MinimumCompressionSize: aws.Int32(0),
+				Version:                aws.String("version"),
+			},
+		},
 	}, nil
 }
 
 func (m mockGetApiGateways) GetResources(ctx context.Context, input *apigateway.GetResourcesInput, optFns ...func(*apigateway.Options)) (*apigateway.GetResourcesOutput, error) {
 	// Return an empty list of API Gateway resources
 	return &apigateway.GetResourcesOutput{
-		Items: []types.Resource{},
+		Items: []types.Resource{
+			{
+				Path:     aws.String("path"),
+				Id:       aws.String("id"),
+				ParentId: aws.String("parentId"),
+			},
+		},
 	}, nil
 }
 
 func (m mockGetApiGateways) GetStages(ctx context.Context, input *apigateway.GetStagesInput, optFns ...func(*apigateway.Options)) (*apigateway.GetStagesOutput, error) {
 	// Return an empty list of API Gateway stages
 	return &apigateway.GetStagesOutput{
-		Item: []types.Stage{},
+		Item: []types.Stage{
+			{
+				DeploymentId: aws.String("deploymentId"),
+				AccessLogSettings: &types.AccessLogSettings{
+					DestinationArn: aws.String("destinationArn"),
+					Format:         aws.String("format"),
+				},
+				TracingEnabled: true,
+				WebAclArn:      aws.String("webAclArn"),
+			},
+		},
 	}, nil
 }
 
@@ -37,6 +63,8 @@ func TestGetApiGateways(t *testing.T) {
 	type args struct {
 		svc APIGatewayGetObjectAPI
 	}
+
+	timeTest, _ := time.Parse("2006-01-02T15:04:05Z", "2019-01-01T00:00:00Z")
 	tests := []struct {
 		name string
 		args args
@@ -47,7 +75,15 @@ func TestGetApiGateways(t *testing.T) {
 			args: args{
 				svc: mockGetApiGateways(nil),
 			},
-			want: []types.RestApi{},
+			want: []types.RestApi{
+				{
+					Id:                     aws.String("id"),
+					CreatedDate:            aws.Time(timeTest),
+					Name:                   aws.String("name"),
+					MinimumCompressionSize: aws.Int32(0),
+					Version:                aws.String("version"),
+				},
+			},
 		},
 	}
 	for _, tt := range tests {
@@ -75,7 +111,13 @@ func TestGetAllResourcesApiGateway(t *testing.T) {
 				svc:   mockGetApiGateways(nil),
 				apiId: "",
 			},
-			want: []types.Resource{},
+			want: []types.Resource{
+				{
+					Path:     aws.String("path"),
+					Id:       aws.String("id"),
+					ParentId: aws.String("parentId"),
+				},
+			},
 		},
 	}
 	for _, tt := range tests {
@@ -103,8 +145,19 @@ func TestGetAllStagesApiGateway(t *testing.T) {
 				svc: mockGetApiGateways(nil),
 				apis: []types.RestApi{
 					{
-						Id: aws.String(""),
+						Id: aws.String("test"),
 					},
+				},
+			},
+			want: []types.Stage{
+				{
+					DeploymentId: aws.String("deploymentId"),
+					AccessLogSettings: &types.AccessLogSettings{
+						DestinationArn: aws.String("destinationArn"),
+						Format:         aws.String("format"),
+					},
+					TracingEnabled: true,
+					WebAclArn:      aws.String("webAclArn"),
 				},
 			},
 		},
@@ -112,7 +165,7 @@ func TestGetAllStagesApiGateway(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := GetAllStagesApiGateway(tt.args.svc, tt.args.apis); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("GetAllStagesApiGateway() = %v, want %v", got, tt.want)
+				t.Errorf("GetAllStagesApiGateway() = %+v, want %+v", got, tt.want)
 			}
 		})
 	}
