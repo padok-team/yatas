669e5de7d52f83318945a420e780028c5e3359bb
StanGirard <stanislasg@padok.fr>
2022-08-26 20:32:34 +0200

    style(report): removed emoji in account line

--- internal/report/report.go
+++ internal/report/report.go
@@ -87,7 +87,7 @@ func CountChecksPassedOverall(checks []yatas.Check) (int, int) {
 func PrettyPrintChecks(checks []yatas.Tests, c *yatas.Config) {
 	flag.Parse()
 	for _, tests := range checks {
-		fmt.Println("\n🔥 Account:", tests.Account, "🔥")
+		fmt.Println("\nAccount:", tests.Account)
 		if *resume {
 			ok, all := CountChecksPassedOverall(tests.Checks)
 			fmt.Printf("\t%d/%d checks passed\n", ok, all)
