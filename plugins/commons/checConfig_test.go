8546be189cac38f6ea8eaf3cf38b4e6d25b1a1af
Stan Girard <girard.stanislas@gmail.com>
2023-04-09 01:12:52 +0200

    feat(types): added type checking check

---
+++ plugins/commons/checConfig_test.go
@@ -0,0 +1,179 @@
+package commons
+
+import (
+	"testing"
+)
+
+func TestCheck_AddResult(t *testing.T) {
+	type fields struct {
+		Name        string
+		Description string
+		Status      string
+		Id          string
+		Results     []Result
+	}
+	type args struct {
+		result Result
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		{
+			name: "add result",
+			fields: fields{
+				Name:        "test",
+				Description: "test",
+				Status:      "OK",
+				Id:          "test",
+				Results:     []Result{},
+			},
+		},
+		{
+			name: "add result",
+			fields: fields{
+				Name:        "test",
+				Description: "test",
+				Status:      "OK",
+				Id:          "test",
+				Results:     []Result{},
+			},
+			args: args{
+				result: Result{
+					Message:    "test",
+					Status:     "OK",
+					ResourceID: "test",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Check{
+				Name:        tt.fields.Name,
+				Description: tt.fields.Description,
+				Status:      tt.fields.Status,
+				Id:          tt.fields.Id,
+				Results:     tt.fields.Results,
+			}
+			c.AddResult(tt.args.result)
+			if c.Status != "OK" {
+				t.Errorf("Status is not OK")
+			}
+		})
+	}
+}
+func TestCheck_AddResultFail(t *testing.T) {
+	type fields struct {
+		Name        string
+		Description string
+		Status      string
+		Id          string
+		Results     []Result
+	}
+	type args struct {
+		result Result
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		{
+			name: "add result",
+			fields: fields{
+				Name:        "test",
+				Description: "test",
+				Status:      "OK",
+				Id:          "test",
+				Results:     []Result{},
+			},
+			args: args{
+				result: Result{
+					Message:    "test",
+					Status:     "FAIL",
+					ResourceID: "test",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Check{
+				Name:        tt.fields.Name,
+				Description: tt.fields.Description,
+				Status:      "OK",
+				Id:          tt.fields.Id,
+				Results:     tt.fields.Results,
+			}
+			c.AddResult(tt.args.result)
+			if c.Status != "FAIL" {
+				t.Errorf("Status is not FAIL")
+			}
+		})
+	}
+}
+
+func TestCheck_InitCheck(t *testing.T) {
+	type fields struct {
+		Name        string
+		Description string
+		Status      string
+		Id          string
+		Results     []Result
+	}
+	type args struct {
+		name        string
+		description string
+		id          string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		{
+			name: "init check",
+			fields: fields{
+				Name:        "test",
+				Description: "test",
+				Status:      "OK",
+				Id:          "test",
+				Results:     []Result{},
+			},
+		},
+		{
+			name: "init check",
+			fields: fields{
+				Name:        "test",
+				Description: "test",
+				Status:      "OK",
+				Id:          "test",
+				Results:     []Result{},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &Check{
+				Name:        tt.fields.Name,
+				Description: tt.fields.Description,
+				Status:      tt.fields.Status,
+				Id:          tt.fields.Id,
+				Results:     tt.fields.Results,
+			}
+			categories := []string{"test"}
+			c.InitCheck(tt.args.name, tt.args.description, tt.args.id, categories)
+			if c.Name != tt.args.name {
+				t.Errorf("Name is not %s", tt.args.name)
+			}
+			if c.Description != tt.args.description {
+				t.Errorf("Description is not %s", tt.args.description)
+			}
+			if c.Id != tt.args.id {
+				t.Errorf("Id is not %s", tt.args.id)
+			}
+		})
+	}
+}
--- plugins/commons/types.go
+++ plugins/commons/types.go
@@ -1,31 +1,101 @@
 package commons
 
 import (
+	"net/rpc"
+	"sync"
 	"time"
 )
 
-type T Check
-
-// Add Result to a check with some logic to update the status of the check
-func (c *Check) AddResult(result Result) {
-	if result.Status == "FAIL" {
-		c.Status = "FAIL"
-	}
-	c.Results = append(c.Results, result)
+// Wrapper struct that holds all the results of the checks for each account
+type Tests struct {
+	Account string  `yaml:"account"` // Account name
+	Checks  []Check `yaml:"checks"`  // Checks
 }
 
-// Initialise a check
-func (c *Check) InitCheck(name, description, id string, categories []string) {
-	c.Name = name
-	c.Description = description
-	c.Status = "OK"
-	c.Id = id
-	c.Categories = categories
-	c.StartTime = time.Now()
+// A check is a test that is run on a resource
+type Check struct {
+	Name        string        `yaml:"name"`        // Name of the check
+	Description string        `yaml:"description"` // Description of the check
+	Status      string        `yaml:"status"`      // Status of the check - OK, FAIL
+	Id          string        `yaml:"id"`          // ID of the check - unique identifier for the check - AWS_IAM_001
+	Categories  []string      `yaml:"categories"`  // Category of the check - Security, Cost, Performance, Fault Tolerance, Operational Excellence, etc ...
+	Results     []Result      `yaml:"results"`     // Results of the check
+	Duration    time.Duration `yaml:"duration"`    // Duration of the check
+	StartTime   time.Time
+	EndTime     time.Time
 }
 
-// End a check by updating the duration and end time
-func (c *Check) EndCheck() {
-	c.EndTime = time.Now()
-	c.Duration = c.EndTime.Sub(c.StartTime)
+type Result struct {
+	Message    string `yaml:"message"`      // Message to display
+	Status     string `yaml:"status"`       // Status of the check
+	ResourceID string `yaml:"resource_arn"` // Resource ID - unique identifier for the resource
+}
+
+type Config struct {
+	Plugins      []Plugin                 `yaml:"plugins"`
+	Ignore       []Ignore                 `yaml:"ignore"`
+	PluginConfig []map[string]interface{} `yaml:"pluginsConfiguration"`
+	Tests        []Tests                  `yaml:"tests"`
+}
+
+// CheckConfig is a struct that contains all the information needed to run a check.
+type CheckConfig struct {
+	Wg          *sync.WaitGroup // Wait group to wait for all the checks to be done
+	Queue       chan Check      // Queue to add the results to
+	ConfigYatas *Config         // Yatas config
+}
+
+type Ignore struct {
+	ID     string   `yaml:"id"`
+	Regex  bool     `yaml:"regex"`
+	Values []string `yaml:"values"`
+}
+
+// Yatas is the interface that we're exposing as a plugin.
+type Yatas interface {
+	Run(c *Config) []Tests
+}
+
+// Here is an implementation that talks over RPC
+type YatasRPC struct{ client *rpc.Client }
+
+// This is the implementation of plugin.Plugin so we can serve/consume this
+//
+// This has two methods: Server must return an RPC server for this plugin
+// type. We construct a YatasRPCServer for this.
+//
+// Client must return an implementation of our interface that communicates
+// over an RPC client. We return YatasRPC for this.
+//
+// Ignore MuxBroker. That is used to create more multiplexed streams on our
+// plugin connection and is a more advanced use case.
+type YatasPlugin struct {
+	// Impl Injection
+	Impl Yatas
+}
+
+// Here is the RPC server that YatasRPC talks to, conforming to
+// the requirements of net/rpc
+type YatasRPCServer struct {
+	// This is the real implementation
+	Impl Yatas
+}
+
+type Plugin struct {
+	Name           string   `yaml:"name"`
+	Enabled        bool     `yaml:"enabled"`
+	Source         string   `yaml:"source"`
+	Type           string   `default:"checks" yaml:"type" `
+	Version        string   `yaml:"version"`
+	Description    string   `yaml:"description"`
+	Exclude        []string `yaml:"exclude"`
+	Include        []string `yaml:"include"`
+	Command        string   `yaml:"command"`
+	Args           []string `yaml:"args"`
+	ExpectedOutput string   `yaml:"expected_output"`
+	ExpectedStatus int      `yaml:"expected_status"`
+
+	// Parsed source attributes
+	SourceOwner string
+	SourceRepo  string
 }
